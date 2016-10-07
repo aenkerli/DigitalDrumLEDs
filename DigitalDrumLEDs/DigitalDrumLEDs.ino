e97a0f3e17f55b392f737d0afaecf48bdbe563aa
Adrian Enkerli <adi_enkerli@hotmail.com>
2016-10-07 09:04:01 +0200

    First file with comments

---
+++ DigitalDrumLEDs/DigitalDrumLEDs.ino
@@ -0,0 +1,15 @@
+/*
+ * Project: Digital Drum LEDs
+ * Autor: Adrian Enkerli
+ * Datum: 07.10.2016
+ * Version: 0.1
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
